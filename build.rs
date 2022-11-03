12a38ec16652f7fb3af0008b84818a92b630a685
Ferry Jérémie <ferryjeremie@free.fr>
2022-11-03 20:15:57 +0100

    cargo clippy suggestions after upgrade to rust 1.65

--- build.rs
+++ build.rs
@@ -11,9 +11,9 @@ fn main() -> Result<(), std::io::Error> {
     let outdir = env!("CARGO_MANIFEST_DIR");
     let name = crate_name!();
     let mut cmd = build_cli(name, crate_version!());
-    generate_to(Bash, &mut cmd, name, &outdir)?;
-    generate_to(Zsh, &mut cmd, name, &outdir)?;
-    generate_to(Fish, &mut cmd, name, &outdir)?;
+    generate_to(Bash, &mut cmd, name, outdir)?;
+    generate_to(Zsh, &mut cmd, name, outdir)?;
+    generate_to(Fish, &mut cmd, name, outdir)?;
 
     Ok(())
 }
